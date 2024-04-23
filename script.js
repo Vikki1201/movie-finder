47c9c5ca5d1789dccc67378cf211dbb0c6090cce
Viktoriia <viktoriia.koyun@yandex.com>
2024-04-23 18:01:40 +0300

    movie finder fixed

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ const Movie = (props) => {
     return (
         <div className="row">
             <div className="col-4 col-md-3 mb-3">
-                <a href="{`https://www.omdbapi.com/title/${imdbID}/`}" target="_blank">
+                <a href={`https://www.omdbapi.com/title/${imdbID}/`} target="_blank">
                     <img src={Poster} className="img-fluid" />
                 </a>
             </div>
             <div className="col-8 col-md-9 mb-3">
-                <a href="{`https://www.omdbapi.com/title/${imdbID}/`}" target="_blank">
+                <a href={`https://www.omdbapi.com/title/${imdbID}/`} target="_blank">
                     <h4>{Title}</h4>
                     <p>{Type} | {Year}</p>
                 </a>
